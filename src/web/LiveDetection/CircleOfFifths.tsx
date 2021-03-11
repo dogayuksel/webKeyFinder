d56a87b32b61c8a322542796c7a15b19c6d4017a
Doga Yuksel <dogayuksel@pm.me>
2021-03-11 21:16:26 +0100

    highlight current result on circleOfFifths

--- src/web/LiveDetection/CircleOfFifths.tsx
+++ src/web/LiveDetection/CircleOfFifths.tsx
@@ -1,7 +1,16 @@
 import { h, createRef, Fragment, Component } from 'preact';
 
-const HEIGHT = 500;
-const INNER_HEIGHT = 340;
+const HORIZONTAL_OFFSET = 500;
+const BORDER_RADIUS = 270;
+const BORDER_THICKNESS = 2;
+const BORDER_COLOR = '#aaa';
+const OUTER_RADIUS = 240;
+const INNER_RING_OFFSET = 25;
+const INNER_RADIUS = 160;
+const NOTE_WIDTH = 55;
+const INNERMOST_RADIUS = 115;
+const PRIMARY_COLOR = '#008DD5';
+const WHITE_COLOR = '#FFF';
 
 const majorKeys = [
   {"C Major": "C" },
@@ -33,6 +42,62 @@ const minorKeys = [
   { "D Minor": "Dm" },
 ];
 
+const InnerSemiCircle = ({ backgroundColor, angleOffset }) => (
+  <div
+    style={{
+      position: 'absolute',
+      transform: `rotate(${angleOffset}deg)`,
+      transformOrigin: 'bottom center',
+      height: `${(INNER_RADIUS + INNER_RING_OFFSET)}px`,
+      width: `${(INNER_RADIUS + INNER_RING_OFFSET) * 2}px`,
+      top: `${BORDER_RADIUS - (INNER_RADIUS + INNER_RING_OFFSET)}px`,
+      left: `${HORIZONTAL_OFFSET - (INNER_RADIUS + INNER_RING_OFFSET)}px`,
+      border: `${OUTER_RADIUS - INNER_RADIUS}px solid ${backgroundColor}`,
+      borderTopLeftRadius: `${(INNER_RADIUS + INNER_RING_OFFSET) * 2}px`,
+      borderTopRightRadius: `${(INNER_RADIUS + INNER_RING_OFFSET) * 2}px`,
+      borderBottom: 'none',
+    }}
+  />
+)
+
+const OuterSemiCircle = ({ backgroundColor, angleOffset }) => (
+  <div
+    style={{
+      position: 'absolute',
+      transform: `rotate(${angleOffset}deg)`,
+      transformOrigin: 'bottom center',
+      height: `${(BORDER_RADIUS)}px`,
+      width: `${BORDER_RADIUS * 2}px`,
+      left: `${HORIZONTAL_OFFSET - (BORDER_RADIUS)}px`,
+      border: `${BORDER_RADIUS - (INNER_RADIUS + INNER_RING_OFFSET)}px solid ${backgroundColor}`,
+      borderTopLeftRadius: `${(BORDER_RADIUS) * 2}px`,
+      borderTopRightRadius: `${(BORDER_RADIUS) * 2}px`,
+      borderBottom: 'none',
+    }}
+  />
+)
+
+const SemiCircleHighlight = ({ result }) => {
+  const majorKeyIndex = majorKeys.findIndex(major => Object.keys(major).some(key => key === result));
+  const minorKeyIndex = minorKeys.findIndex(minor => Object.keys(minor).some(key => key === result));
+  if (majorKeyIndex >= 0) {
+    return (
+      <>
+        <OuterSemiCircle backgroundColor={PRIMARY_COLOR} angleOffset={majorKeyIndex * 30 - (90 - 15)} />
+        <OuterSemiCircle backgroundColor={WHITE_COLOR} angleOffset={(majorKeyIndex - 1) * 30 - (90 - 15)} />
+      </>
+    );
+  } else if (minorKeyIndex >= 0) {
+    return (
+      <>
+        <InnerSemiCircle backgroundColor={PRIMARY_COLOR} angleOffset={minorKeyIndex * 30 - (90 - 15)} />
+        <InnerSemiCircle backgroundColor={WHITE_COLOR} angleOffset={(minorKeyIndex - 1) * 30 - (90 - 15)} />
+      </>
+    );
+  }
+  return null;
+};
+
 class CircleOfFifths extends Component<({ result?: string })> {
   render() {
     return (
@@ -42,36 +107,52 @@ class CircleOfFifths extends Component<({ result?: string })> {
         }}
       >
         <div style={{position: 'relative' }}>
+          <SemiCircleHighlight result={this.props.result} />
           {majorKeys.map((major, index) =>
             Object.entries(major).map(([key, value]) => (
-              <div style={{
-                     left: `${HEIGHT}px`,
-                     height: `${HEIGHT / 2}px`,
-                     transform: `rotate(${index * 30}deg)`,
-                     transformOrigin: 'bottom center',
-                     position: 'absolute',
-                   }}>
+              <>
+                <div style={{
+                       top: `${BORDER_RADIUS - OUTER_RADIUS}px`,
+                       left: `${HORIZONTAL_OFFSET - NOTE_WIDTH / 2}px`,
+                       height: `${OUTER_RADIUS}px`,
+                       width: `${NOTE_WIDTH}px`,
+                       transform: `rotate(${index * 30}deg)`,
+                       transformOrigin: 'bottom center',
+                       position: 'absolute',
+                     }}>
+                  <div
+                    style={{
+                      textAlign: 'center',
+                      transform: `rotate(${-index * 30}deg)`,
+                      fontWeight: 'bold'
+                    }}
+                  >
+                    {value}
+                  </div>
+                </div>
                 <div
                   style={{
-                    padding: '0.2em',
-                    backgroundColor: key === this.props.result ? '#008DD5' : '#fff',
-                    borderRadius: '0.1em',
-                    transform: `rotate(${-index * 30}deg)`,
-                    fontWeight: 'bold'
+                    left: `${HORIZONTAL_OFFSET - BORDER_THICKNESS / 2}px`,
+                    width: `${BORDER_THICKNESS}px`,
+                    height: `${BORDER_RADIUS}px`,
+                    backgroundColor: `${BORDER_COLOR}`,
+                    transform: `rotate(${index * 30 - 15}deg)`,
+                    transformOrigin: 'bottom center',
+                    position: 'absolute',
                   }}
-                >
-                  {value}
-                </div>
-              </div>
+                />
+              </>
             ))
           )}
           {minorKeys.map((minor, index) =>
             Object.entries(minor).map(([key, value]) => (
               <div
                 style={{
-                  top: `${(HEIGHT - INNER_HEIGHT) / 2}px`,
-                  left: `${HEIGHT}px`,
-                  height: `${INNER_HEIGHT / 2}px`,
+                  width: `${NOTE_WIDTH}px`,
+                  textAlign: 'center',
+                  top: `${(BORDER_RADIUS - INNER_RADIUS)}px`,
+                  left: `${HORIZONTAL_OFFSET - NOTE_WIDTH / 2}px`,
+                  height: `${INNER_RADIUS}px`,
                   transform: `rotate(${index * 30}deg)`,
                   transformOrigin: 'bottom center',
                   position: 'absolute',
@@ -79,9 +160,6 @@ class CircleOfFifths extends Component<({ result?: string })> {
               >
                 <div
                   style={{
-                    padding: '0.2em',
-                    backgroundColor: key === this.props.result ? '#008DD5' : '#fff',
-                    borderRadius: '0.1em',
                     transform: `rotate(${-index * 30}deg)`,
                     fontSize: "0.8rem"
                   }}
@@ -91,6 +169,39 @@ class CircleOfFifths extends Component<({ result?: string })> {
               </div>
             ))
           )}
+          <div
+            style={{
+              position: 'absolute',
+              borderRadius: `${BORDER_RADIUS}px`,
+              border: `${BORDER_THICKNESS}px solid ${BORDER_COLOR}`,
+              height: `${BORDER_RADIUS * 2}px`,
+              width: `${BORDER_RADIUS * 2}px`,
+              left: `${HORIZONTAL_OFFSET - BORDER_RADIUS}px`,
+            }}
+          />
+          <div
+            style={{
+              position: 'absolute',
+              borderRadius: `${BORDER_RADIUS}px`,
+              border: `${BORDER_THICKNESS}px solid ${BORDER_COLOR}`,
+              height: `${(INNER_RADIUS + INNER_RING_OFFSET) * 2}px`,
+              width: `${(INNER_RADIUS + INNER_RING_OFFSET) * 2}px`,
+              top: `${BORDER_RADIUS - (INNER_RADIUS + INNER_RING_OFFSET)}px`,
+              left: `${HORIZONTAL_OFFSET - (INNER_RADIUS + INNER_RING_OFFSET)}px`,
+            }}
+          />
+          <div
+            style={{
+              position: 'absolute',
+              borderRadius: `${BORDER_RADIUS}px`,
+              border: `${BORDER_THICKNESS}px solid ${BORDER_COLOR}`,
+              height: `${INNERMOST_RADIUS * 2}px`,
+              width: `${INNERMOST_RADIUS * 2}px`,
+              top: `${BORDER_RADIUS - INNERMOST_RADIUS}px`,
+              left: `${HORIZONTAL_OFFSET - INNERMOST_RADIUS}px`,
+              backgroundColor: `${WHITE_COLOR}`,
+            }}
+          />
         </div>
       </div>
     );
