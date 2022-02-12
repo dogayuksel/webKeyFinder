49a6247014a41682a3b152e55a6f0fd6e39f7a30
Doga Yuksel <dogayuksel@pm.me>
2022-02-12 13:24:56 +0100

    run on chrome on ci

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,11 +1,9 @@
 module.exports = (on, config) => {
   on('before:browser:launch', (browser = {}, launchOptions) => {
-    if (browser.name === 'chromium') {
-      launchOptions.args.push('--use-fake-device-for-media-stream')
-      launchOptions.args.push(
-        '--use-file-for-fake-audio-capture='
-          + 'cypress/fixtures/Mindseye - Interstellar (90sec mic record).wav')
-    }
+    launchOptions.args.push('--use-fake-device-for-media-stream')
+    launchOptions.args.push(
+      '--use-file-for-fake-audio-capture='
+        + 'cypress/fixtures/Mindseye - Interstellar (90sec mic record).wav')
     return launchOptions
   })
 }
