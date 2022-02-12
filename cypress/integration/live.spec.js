1ced7175cd2c30a3ea5a09ba753085ac741b5f65
Doga Yuksel <dogayuksel@pm.me>
2022-02-12 11:04:08 +0100

    add a cypress test for live key detection

--- cypress/integration/live.spec.js
+++ cypress/integration/live.spec.js
@@ -10,4 +10,20 @@ describe('live key analysis', () => {
       cy.get('header h1').should('have.text', 'Live Key Detection')
     })
   })
+
+  describe('key detection', () => {
+    it('should perform key detection', () => {
+      cy.visit('localhost:3000')
+      cy.get('main input').first().click()
+      cy.get('input[value="Key detection engine running"]')
+      cy.get('input[value="Start Key Detection"]').click()
+      cy.get('div').contains('Progressive Result', { timeout: 20000 })
+      cy.wait(30000)
+      cy.get('div').contains('Progressive Result: Gm')
+      cy.wait(10000)
+      cy.get('div').contains('Progressive Result: Gm')
+      cy.get('input[value="End Key Detection"]').click()
+      cy.get('div').contains('Final Result: Gm')
+    });
+  });
 })
