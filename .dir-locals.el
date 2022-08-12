51d1afb364ab568128e1bbe147de00b69c0eb709
Doga Yuksel <dogayuksel@pm.me>
2022-08-12 18:36:58 +0200

    consume key-finder-wasm from key-finder-web module

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil . ((projectile-project-compilation-cmd . "source ~/code/emsdk/emsdk_env.sh 2>/dev/null && make")
-         (projectile-project-run-cmd . "source ~/code/emsdk/emsdk_env.sh 2>/dev/null && make && make serve"))))
--- packages/key-finder-web/rollup.config.js
+++ packages/key-finder-web/rollup.config.js
@@ -2,19 +2,17 @@ import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import OMT from "@surma/rollup-plugin-off-main-thread";
 import css from 'rollup-plugin-css-only';
-import { terser } from "rollup-plugin-terser";
 
 export default {
-  input: 'src/web/index.tsx',
+  input: 'src/index.tsx',
   output: {
     dir: 'dist',
-    format: 'esm'
+    format: 'esm',
   },
   plugins: [
     OMT(),
     resolve({ browser: true }),
     typescript(),
-    terser(),
     css({ output: 'index.css' }),
   ],
 };
