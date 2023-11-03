c6a00e8b7844d154d2890009e43d1ac348888fdd
Doğa Yüksel <dogayuksel@pm.me>
2023-11-03 19:46:08 +0100

    turn video back on, log console errors

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  video: true,
   e2e: {
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       on('before:browser:launch', (browser = {}, launchOptions) => {
+        launchOptions.args.push('--use-fake-ui-for-media-stream');
         launchOptions.args.push('--use-fake-device-for-media-stream');
         launchOptions.args.push(
           '--use-file-for-fake-audio-capture=' +
