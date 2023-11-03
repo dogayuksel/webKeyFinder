d2815d33f87b05398b665773453a0a37c435d757
Doğa Yüksel <dogayuksel@pm.me>
2023-11-03 12:32:20 +0100

    try 'no-sandbox' to circumvent no ssl and mic permissions

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -4,6 +4,8 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       on('before:browser:launch', (browser = {}, launchOptions) => {
+        // chrome rejects mic permissions on non-https
+        launchOptions.args.push('--no-sandbox');
         launchOptions.args.push('--use-fake-device-for-media-stream');
         launchOptions.args.push(
           '--use-file-for-fake-audio-capture=' +
