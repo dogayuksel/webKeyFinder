f8ccd12a83406c725a14537dcbd3f37200ee2648
Doğa Yüksel <dogayuksel@pm.me>
2023-12-15 16:03:10 +0100

    double click to ignore slient addModule(recorderWorkletURL) fail

--- cypress/e2e/live.cy.js
+++ cypress/e2e/live.cy.js
@@ -14,11 +14,14 @@ describe('live key analysis', () => {
   describe('key detection', () => {
     it('should perform key detection', () => {
       cy.visit('/');
-      cy.get('input[value="Route sound to key detection engine"]').click();
+      // addModule(recorderWorkletURL) silently fails on first try, hence double click
+      cy.get('input[value="Route sound to key detection engine"]')
+        .click()
+        .click();
       cy.get('input[value="Key detection engine running"]', { timeout: 10000 });
       cy.get('input[value="Start Key Detection"]').click();
       cy.get('div').contains('Progressive Result', { timeout: 20000 });
-      cy.wait(30000);
+      cy.wait(80000);
       cy.get('div').contains('Progressive Result: Gm');
       cy.wait(10000);
       cy.get('div').contains('Progressive Result: Gm');
