bbe419205afe061fd3b22a605a41c75be61b1f71
Doğa Yüksel <dogayuksel@pm.me>
2025-05-25 16:11:01 +0200

    update flake stable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "webkeyfinder development shell";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
